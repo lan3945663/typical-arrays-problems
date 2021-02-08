a1928a851ad6cd1a4669209b3b930eb05d49da1e
lan3945663 <lan3945663@gmail.com>
2021-02-08 21:34:38 +0300

    feat: add max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-
 exports.min = function min (array) {
+  if(array && array.length > 0) {
+    let temp = array[0];
+
+    array.forEach((el, i) => {
+      if(el < temp) {
+        temp = array[i];
+      }
+    })
+
+    return temp;
+  }
+  
   return 0;
 }
 
 exports.max = function max (array) {
+  if(array && array.length > 0) {
+    let temp = array[0];
+
+    for(let i = 0; i < array.length; i++) {
+      if(array[i] > temp) {
+        temp = array[i];
+      }
+    }
+
+    return temp;
+  }
+  
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if(array && array.length > 0) {
+    let sum = 0;
+
+    for(let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+
+    // II variant
+
+    // const res = array.reduce((sum, current) => {
+    //   return sum + current;
+    // }, 0);
+
+    const avg = sum/array.length;
+
+    return avg
+  }
+
   return 0;
 }
